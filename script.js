badf83d1a66bbbfc05556dd0cd3cf1a61000c89c
Jnadaldevwwm <julien.devwwm79@gmail.com>
2020-09-14 12:02:52 +0200

    maj 1,0001

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-function hamburger(e){
-    e.preventDefault();
+function hamburger(){
     var zoneLiens = document.getElementById('liens');
     if(zoneLiens.style.display === "block"){
         zoneLiens.style.display = 'none';
     } else {
         zoneLiens.style.display = 'block';
     }
+}
+
+var
+	carousel = document.querySelector('.carousel'),
+	figure = carousel.querySelector('figure'),
+	nav = carousel.querySelector('nav'),
+	numImages = figure.childElementCount,
+	theta =  2 * Math.PI / numImages,
+	currImage = 0
+;
+	
+nav.addEventListener('click', onClick, true);
+
+function onClick(e) {
+	e.stopPropagation();
+	
+	var t = e.target;
+	if (t.tagName.toUpperCase() != 'BUTTON')
+		return;
+	
+	if (t.classList.contains('next')) {
+		currImage++;
+	}
+	else {
+		currImage--;
+	}
+	
+	figure.style.transform = `rotateY(${currImage * -theta}rad)`;
 }
